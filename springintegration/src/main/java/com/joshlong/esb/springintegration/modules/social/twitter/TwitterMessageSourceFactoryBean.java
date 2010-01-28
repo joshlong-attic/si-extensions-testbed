938680a1c192ea2df3118be4607ddc17f4528e08
starbuxman <starbuxman@096ce012-0bc9-11df-acff-d76401ec9c37>
2010-01-28 09:05:02 +0000

    oscontrib-3

---
+++ springintegration/src/main/java/com/joshlong/esb/springintegration/modules/social/twitter/TwitterMessageSourceFactoryBean.java
@@ -0,0 +1,46 @@
+package com.joshlong.esb.springintegration.modules.social.twitter;
+
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+
+public class TwitterMessageSourceFactoryBean  extends AbstractFactoryBean<TwitterMessageSource> {
+
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    private String channel;
+
+    @Override
+    public Class<? extends TwitterMessageSource> getObjectType() {
+        return TwitterMessageSource.class ;
+    }
+
+    @Override
+    protected TwitterMessageSource createInstance() throws Exception {
+        return null;
+    }
+}
---
+++ springintegration/src/main/java/com/joshlong/esb/springintegration/modules/social/twitter/config/TwitterNamespaceHandler.java
@@ -0,0 +1,45 @@
+package com.joshlong.esb.springintegration.modules.social.twitter.config;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.integration.config.xml.AbstractPollingInboundChannelAdapterParser;
+import org.springframework.integration.config.xml.IntegrationNamespaceUtils;
+import org.w3c.dom.Element;
+
+
+public class TwitterNamespaceHandler extends NamespaceHandlerSupport {
+
+    private static final String PACKAGE_NAME = "com.joshlong.esb.springintegration.modules.social.twitter";
+
+    public void init() {
+        registerBeanDefinitionParser("inbound-channel-adapter", new TwitterMessageSourceBeanDefinitionParser());
+    }
+
+    private class TwitterMessageSourceBeanDefinitionParser extends AbstractPollingInboundChannelAdapterParser {
+        @Override
+        protected String parseSource(Element element, ParserContext parserContext) {
+            BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(
+                    PACKAGE_NAME + ".config.FileReadingMessageSourceFactoryBean");
+            IntegrationNamespaceUtils.setReferenceIfAttributeDefined(builder, element, "comparator");
+            IntegrationNamespaceUtils.setValueIfAttributeDefined(builder, element, "directory");
+            IntegrationNamespaceUtils.setValueIfAttributeDefined(builder, element, "auto-create-directory");
+       //     String filterBeanName = this.registerFileListFilter(element, parserContext);
+         //   builder.addPropertyReference("filter", filterBeanName);
+            return BeanDefinitionReaderUtils.registerWithGeneratedName(builder.getBeanDefinition(), parserContext.getRegistry());
+
+        }
+
+        /*   @Override
+        protected void doParse(Element element, BeanDefinitionBuilder builder) {
+
+        }
+
+        @Override
+        protected Class getBeanClass(Element element) {
+         return TwitterMessageSourceFactoryBean.class ; 
+        }*/
+
+    }
+}
