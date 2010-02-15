4334d5d3264c53a1abe7cd3ec2b7170d2e7d143f
starbuxman <starbuxman@096ce012-0bc9-11df-acff-d76401ec9c37>
2010-02-15 02:20:30 +0000

    updted license

--- native_fsmonitor_c_code/fsmon.c
+++ native_fsmonitor_c_code/fsmon.c
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 the original author or authors
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
 #include <jni.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -44,7 +59,7 @@ JNIEXPORT void JNICALL Java_com_joshlong_esb_springintegration_modules_nativefs_
 
 	
 // cache these
-	jclass cls = (*env)->GetObjectClass(env, obj);
+	jclass cls = (t*env)->GetObjectClass(env, obj);
 	jmethodID mid = (*env)->GetMethodID(env, cls, "nativeFileRecieved", "(Ljava/lang/String;)V");
  	/* to figure out method signature, i ran:  javap -s -p -classpath . com.joshlong.esb.springintegration.modules.nativefs.NativeFileSystemMonitor  */
 	if( mid == 0 ) {
