703c2d6d8e4ec9a7fe3bb02b6cb0b56c29e4af52
starbuxman <starbuxman@096ce012-0bc9-11df-acff-d76401ec9c37>
2010-02-16 06:23:00 +0000

    SESIA-22

---
+++ springintegration/src/main/java/com/joshlong/esb/springintegration/modules/net/feed/Main.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2010 the original author or authors
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.joshlong.esb.springintegration.modules.net.feed;
+
+/**
+ * @author <a href="mailto:josh@joshlong.com">Josh Long</a> There are two goals here: - establish a mechanism to
+ *         'monitor' an RSS/ATOM resource and publish events as inbound events - to 'update' an RSS/ATOM resource by
+ *         taking outbound messages and updating a resource
+ */
+public class Main {
+
+    static public void rssReading1() throws Throwable {
+
+    }
+
+    static public void main(String[] ar) throws Throwable {
+
+        rssReading1();
+    }
+
+}
