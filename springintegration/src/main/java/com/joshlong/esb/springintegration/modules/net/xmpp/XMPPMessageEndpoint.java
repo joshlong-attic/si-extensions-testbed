e2f704d520147a3ed666b84a0ce39f8eda5994e3
starbuxman <starbuxman@096ce012-0bc9-11df-acff-d76401ec9c37>
2010-02-20 23:04:16 +0000

    SESIA-14

--- springintegration/src/main/java/com/joshlong/esb/springintegration/modules/net/xmpp/XMPPMessageEndpoint.java
+++ springintegration/src/main/java/com/joshlong/esb/springintegration/modules/net/xmpp/XMPPMessageEndpoint.java
@@ -72,7 +72,7 @@ public class XMPPMessageEndpoint extends AbstractEndpoint implements Lifecycle {
 
     @Override
     protected void onInit() throws Exception {
-        assert xmppConnection != null : "the xmppCnnection shouldn't be null";
+     //   assert xmppConnection != null : "the xmppCnnection shouldn't be null";
         channelTemplate.afterPropertiesSet();
 
         XMPPConnectionFactory xmppConnectionFactory = new XMPPConnectionFactory(this.getUser(), this.getPassword(), this.getHost(), this.getServiceName(), this.getResource(),
